5662eb8019dce0a30d65fee89d50fefe0691c50c
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-06-26 22:53:25 +0200

    feat: setup dap for rust

--- configs/nvim/after/plugin/dap.lua
+++ configs/nvim/after/plugin/dap.lua
@@ -6,6 +6,41 @@ require("dapui").setup()
 require("nvim-dap-virtual-text").setup({
 })
 
+local mason_registry = require("mason-registry")
+local codelldb = mason_registry.get_package("codelldb")
+local codelldb_install_path = codelldb:get_install_path()
+local codelldb_exe_path = codelldb_install_path .. "\\adapter\\codelldb.exe"
+
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    command = codelldb_exe_path,
+    args = {"--port", "${port}"},
+
+    -- On windows you may have to uncomment this:
+    detached = false,
+  }
+}
+
+dap.configurations.rust = {
+    {
+        type = "codelldb",
+        request = "launch",
+        name = "exec",
+        command = "rust",
+        cwd = '${workspaceFolder}',
+        program = function()
+            local cwd = vim.fn.getcwd()
+            local workspace_dir_name = cwd:gmatch("[^\\]+$")()
+
+            return cwd .. '/target/debug/' .. workspace_dir_name .. ".exe"
+        end,
+        exitAfterTaskReturns = false,
+        debugAutoInterpretAllModules = false
+    }
+}
+
 dap.listeners.before.attach.dapui_config = function()
   ui.open()
 end
