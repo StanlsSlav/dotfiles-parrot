ed75e0fb8b20bd04f9c4546c018fbc72cf951561
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-05 00:07:33 +0200

    style: enable transparent background for catppuccin

--- configs/nvim/after/plugin/catppuccin.lua
+++ configs/nvim/after/plugin/catppuccin.lua
@@ -1,4 +1,6 @@
 require("catppuccin").setup {
+    transparent_background = true,
+    show_end_of_buffer = true,
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     no_italic = true,  -- Force no italic
     integrations = {
