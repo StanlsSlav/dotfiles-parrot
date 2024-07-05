68e7f9ca32227fd258c0f878c543b03e9a30a4a4
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-06 01:03:14 +0200

    fix: replace invalid syntax

--- setup_scripts/zsh.sh
+++ setup_scripts/zsh.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-apt install zsh
+/usr/bin/sudo /usr/bin/apt install zsh
 
-CONFIGS_DIR = ../configs
-OMZ_HOME = $HOME/.oh-my-zsh
+CONFIGS_DIR=../configs
+OMZ_HOME=$HOME/.oh-my-zsh
 
-[[ -z test $OMZ_HOME ]]
-    || sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[[ -d $OMZ_HOME ]] \
+    || /bin/sh -c "$(/usr/bin/wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/romkatv/powerlevel10k $OMZ_HOME/custom/themes &
+/usr/bin/git clone https://github.com/romkatv/powerlevel10k $OMZ_HOME/custom/themes &
 
-cp -b $CONFIGS_DIR/.zshrc $HOME
-cp -b $CONFIGS_DIR/.p10k.zsh $HOME
+/usr/bin/cp -b $CONFIGS_DIR/.zshrc $HOME
+/usr/bin/cp -b $CONFIGS_DIR/.p10k.zsh $HOME
