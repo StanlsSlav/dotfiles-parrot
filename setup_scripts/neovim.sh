4fef05499efd59007aa8bf4fb0ea19d1b20faf50
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-07 01:19:14 +0200

    fix: replace snap install with gh and add link to bin

--- setup_scripts/neovim.sh
+++ setup_scripts/neovim.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-sudo snap install nvim --classic
-read -p "Copy the files instead of symlink?: [y]es " input
+/usr/bin/gh release download -R neovim/neovim -p nvim-linux64.tar.gz -D $HOME/Downloads
+/usr/bin/tar xzf $HOME/Downloads/nvim-linux64.tar.gz
+/usr/bin/sudo /usr/bin/mv $HOME/Downloads/nvim-linux64 /opt
+
+/usr/bin/read -p "Copy the files instead of symlink?: [y]es " input
 
 [[ -d $HOME/.config ]] \
-    || mkdir $HOME/.config
+    || /usr/bin/mkdir $HOME/.config
 
 NVIM_CONF_DIR=$PWD/../configs/nvim
 case $input in
     'y')
-        cp -br $NVIM_CONF_DIR $HOME/.config/ 2>/dev/null
+        /usr/bin/cp -br $NVIM_CONF_DIR $HOME/.config/ 2>/dev/null
     ;;
     
     *)
-	ln -s $NVIM_CONF_DIR $HOME/.config/ 2>/dev/null
+	/usr/bin/ln -s $NVIM_CONF_DIR $HOME/.config/ 2>/dev/null
     ;;
 esac
+
+/usr/bin/sudo /usr/bin/ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
