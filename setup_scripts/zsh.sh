e9b3e56e327125da6f2714e84bf0a1e77f134a2e
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-07 01:17:42 +0200

    fix: replace https git clone with ssh clone

--- setup_scripts/zsh.sh
+++ setup_scripts/zsh.sh
@@ -8,7 +8,8 @@ OMZ_HOME=$HOME/.oh-my-zsh
 [[ -d $OMZ_HOME ]] \
     || /bin/sh -c "$(/usr/bin/wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-/usr/bin/git clone https://github.com/romkatv/powerlevel10k $OMZ_HOME/custom/themes &
+/usr/bin/git clone git:romkatv/powerlevel10k $OMZ_HOME/custom/themes/powerlevel10k
+/usr/bin/git clone git:zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 
-/usr/bin/cp -b $CONFIGS_DIR/.zshrc $HOME
-/usr/bin/cp -b $CONFIGS_DIR/.p10k.zsh $HOME
+/usr/bin/cp -b $CONFIGS_DIR/.zshrc $HOME &
+/usr/bin/cp -b $CONFIGS_DIR/.p10k.zsh $HOME &
