3cbfab2ed6ff8a008859386d34886b9ddc1c6d4c
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-07 19:02:22 +0200

    feat: add apt packages to install

---
+++ setup_scripts/apt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+/usr/bin/sudo /usr/bin/apt -y update
+/usr/bin/sudo /usr/bin/apt -y upgrade
+
+if [[ $(uname -a) != *WSL2* ]]; then
+    /usr/bin/sudo /usr/bin/apt -y install \
+        containerd.io \
+        docker.io \
+        exiftool \
+        git \
+        openvpn \
+        binwalk \
+        fcrackzip \
+        nmap \
+        wireshark \
+        whatweb \
+        p7zip
+fi
+
+/usr/bin/sudo /usr/bin/apt -y install \
+    jq \
+    clang \
+    unzip
+
+/usr/bin/sudo /usr/bin/apt -y remove --purge
