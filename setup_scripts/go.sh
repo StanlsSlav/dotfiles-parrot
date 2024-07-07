f20ac34c79576edc218edc8b5d1b1d40230b7146
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-07 22:59:35 +0200

    feat: add go install

---
+++ setup_scripts/go.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $(uname -a) == *"WSL"* ]]; then
+    echo "Skipping go installation on WSL"
+    exit 0
+fi
+
+/usr/bin/wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz -O /tmp/go.tar.gz
+/usr/bin/sudo /usr/bin/tar xzf /tmp/go.tar.gz -C /opt
+
+/usr/bin/sudo /usr/bin/ln -s /opt/go/bin/go /usr/local/bin/go
+
+/usr/local/bin/go install github.com/ffuf/ffuf/v2@latest
