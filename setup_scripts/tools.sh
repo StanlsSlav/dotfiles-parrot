dcb7ef165746c25bd98290b989f695557bdbcc58
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-09 01:16:28 +0200

    feat: add other install scripts to main tools.sh

--- setup_scripts/tools.sh
+++ setup_scripts/tools.sh
@@ -7,3 +7,9 @@
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 	&& sudo apt update \
 	&& sudo apt install gh -y
+
+./apt.sh
+./cargo.sh
+./github.sh
+./go.sh
+./other.sh
