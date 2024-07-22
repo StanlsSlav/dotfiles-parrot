fb199fbed423cdcb07fefcd600fe24c6912f9b4c
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-22 18:33:09 +0200

    feat: do apt update each 2nd day

--- setup_scripts/apt.sh
+++ setup_scripts/apt.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-/usr/bin/sudo /usr/bin/apt -y update
-/usr/bin/sudo /usr/bin/apt -y upgrade
+echo "[?] APT install"
+
+current_date=$(/usr/bin/date -d "now" +%s)
+last_apt_update=$(/usr/bin/stat /var/cache/apt/pkgcache.bin -c %y)
+
+sec_diff=$((current_date - last_apt_update))
+
+days_diff = $((sec_diff / 86400))
+
+if [[ $days_diff -gt 2 ]]; then
+    echo "[?] Updating and upgrading APT packages"
+    /usr/bin/sudo /usr/bin/apt -y update
+    /usr/bin/sudo /usr/bin/apt -y upgrade
+else
+    echo "[?] Skipping APT update and upgrade"
+end
+
 
 if [[ $(uname -a) == *WSL2* ]]; then
     /usr/bin/sudo /usr/bin/apt -y install \
