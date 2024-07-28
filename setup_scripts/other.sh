097963cb61fd1515f46b3358dde2ac160464cf9a
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-28 18:01:38 +0200

    refactor: install node when needed

--- setup_scripts/other.sh
+++ setup_scripts/other.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-/usr/bin/echo "[?] Node install"
-LASTEST_NODE_PACKAGE=$(/usr/bin/curl https://nodejs.org/dist/latest-iron/ -s | /usr/bin/grep '.*linux-x64.*gz' | /usr/bin/cut -d '"' -f2)
-/usr/bin/wget https://nodejs.org/dist/latest-iron/$LASTEST_NODE_PACKAGE -O $HOME/Downloads/$LASTEST_NODE_PACKAGE
-/usr/bin/sudo /usr/bin/tar -xzf $HOME/Downloads/$LASTEST_NODE_PACKAGE -C /opt
+CURRENT_NODE_VERSION=$(node --version 2>/dev/null)
+LASTEST_NODE_PACKAGE=$(/usr/bin/curl https://nodejs.org/dist/latest-iron/ -s \
+    | /usr/bin/grep -o 'node[^"]*linux-x64.tar.gz' \
+    | /usr/bin/tail -1)
 
-NODE_PACKAGE=$(echo $LASTEST_NODE_PACKAGE | sed 's/.tar.gz//')
-/usr/bin/sudo /usr/bin/mv /opt/$NODE_PACKAGE /opt/node
-/usr/bin/sudo /usr/bin/ln -s /opt/node/bin/node /usr/local/bin/node
+install_node=$([[ $LASTEST_NODE_PACKAGE != *$CURRENT_NODE_VERSION* ]] && echo "true")
+
+if [[ $install_node || ! $(command -v node) ]]; then
+    /usr/bin/echo "[?] Node install"
+    /usr/bin/wget "https://nodejs.org/dist/latest-iron/$LASTEST_NODE_PACKAGE" \
+        -O "/tmp/$LASTEST_NODE_PACKAGE"
+    /usr/bin/sudo /usr/bin/tar xzf "/tmp/$LASTEST_NODE_PACKAGE" -C /opt
+
+    NODE_PACKAGE=$(echo $LASTEST_NODE_PACKAGE | sed 's/.tar.gz//')
+    /usr/bin/sudo /usr/bin/mv /opt/$NODE_PACKAGE /opt/node
+    /usr/bin/sudo /usr/bin/ln -s /opt/node/bin/node /usr/local/bin/node
+else
+   /usr/bin/echo "[?] Skipping Node install"
+fi
 
 if [[ $(/usr/bin/uname -a) == *WSL* ]]; then
-    echo "[?] Skipping other attack tools install"
+    /usr/bin/echo "[?] Skipping attack tools install"
     exit 0
 fi
 
@@ -25,8 +36,8 @@ STEGSOLVE_URL="http://www.caesum.com/handbook/Stegsolve.jar"
 /usr/bin/sudo /usr/bin/chmod 0755 /opt/stegsolve.jar
 
 /usr/bin/echo "[?] Burpsuite install"
-BURPSUITE_URL="https://portswigger.net/burp/releases/startdownload?product=community&type=Jar"
-/usr/bin/sudo /usr/bin/wget $BURPSUITE_URL -O /usr/share/burpsuite/burpsuite.jar
+BURP_URL="https://portswigger.net/burp/releases/startdownload?product=community&type=Jar"
+/usr/bin/sudo /usr/bin/wget $BURP_URL -O /usr/share/burpsuite/burpsuite.jar
 /usr/bin/sudo /usr/bin/chmod 0755 /usr/share/burpsuite/burpsuite.jar
 
 /usr/bin/echo "[?] IDA install"
@@ -34,9 +45,12 @@ IDA_URL='https://out7.hex-rays.com/files/idafree84_linux.run'
 /usr/bin/sudo /usr/bin/wget $IDA_URL -O /opt/idafree84_linux.run
 /usr/bin/sudo /usr/bin/chmod 0755 /opt/idafree84_linux.run
 
-/usr/bin/sudo /opt/idafree84_linux.run --debuglevel 3 --prefix /opt/idafree-8.4 --mode unattended
+/usr/bin/sudo /opt/idafree84_linux.run \
+    --debuglevel 3 --prefix /opt/idafree-8.4 --mode unattended
 
-/usr/bin/sudo /usr/bin/apt-get install -y python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+/usr/bin/sudo /usr/bin/apt-get install -y \
+    python3 python3-pip python3-dev git \
+    libssl-dev libffi-dev build-essential
 
 /usr/bin/sudo /usr/bin/pip3 install pip pwntools pwncat-cs
 
