b46a2be3711ff5059b440b436ffc169839fc4d50
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-28 14:26:17 +0200

    chore: uncomment script installs

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "[?] Install script"
 ./setup_scripts/tools.sh
-# ./setup_scripts/dconf.sh
-# ./setup_scripts/tmux.sh
-# ./setup_scripts/zsh.sh
-# ./setup_scripts/neovim.sh
+./setup_scripts/dconf.sh
+./setup_scripts/tmux.sh
+./setup_scripts/zsh.sh
+./setup_scripts/neovim.sh
