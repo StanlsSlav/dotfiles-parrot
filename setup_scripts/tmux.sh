88c195ce79b48ce785c3709806ea96f49fee6823
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-28 14:22:45 +0200

    fix: skip tpm install when present

--- setup_scripts/tmux.sh
+++ setup_scripts/tmux.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-apt install -y tmux
+/usr/bin/echo "[?] TMUX setup"
 
-cp -b ../configs/.tmux.conf ~/.tmux.conf
-cp ../configs/files/.checktun0 ~/.checktun0
+/usr/bin/sudo /usr/bin/apt install -y tmux
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+/usr/bin/cp -b ./configs/.tmux.conf $HOME
+/usr/bin/cp ./configs/files/.checktun0 $HOME
 
-echo "Install the plugins via Leader+I"
+tpm_dir=($HOME/.tmux/plugins/tpm)
+if [[ -d $tpm_dir ]]; then
+    /usr/bin/echo "[?] Skipping TPM install"
+else
+    /usr/bin/echo "[+] Installing TPM"
+    /usr/bin/git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+/usr/bin/echo "[?] Install the plugins via Leader+I"
