a7e4960f2ed64ba2d23417bd7e3ab50f33c62f5a
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-28 14:08:51 +0200

    feat: skip dconf setup when not possible

--- setup_scripts/dconf.sh
+++ setup_scripts/dconf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "[?] Dconf setup"
-dconf load /org/mate/terminal/profiles/default/ < ../configs/dconf-default-profile
 
+if [[ -z $(command -v dconf) ]]; then
+    echo "[?] Dconf not found, skipping setup..."
+fi
+
+dconf load /org/mate/terminal/profiles/default/ < ../configs/dconf-default-profile
