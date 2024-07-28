4eefde903403bdf62a4a906fcebc2c02b526649b
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-28 14:44:46 +0200

    feat: install make

--- setup_scripts/tools.sh
+++ setup_scripts/tools.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-echo "[?] Tools install"
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+/usr/bin/echo "[?] Tools install"
 
-./apt.sh
-./cargo.sh
-./github.sh
-./go.sh
-./other.sh
+if [[ ! $(command -v gh) ]]; then
+    /usr/bin/echo "[?] GH CLI not found, installing..."
+    (type -p wget >/dev/null || (/usr/bin/sudo /usr/bin/apt update && /usr/bin/sudo /usr/bin/apt-get install wget -y)) \
+        && /usr/bin/sudo /usr/bin/mkdir -p -m 755 /etc/apt/keyrings \
+        && /usr/bin/wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | /usr/bin/sudo /usr/bin/tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+        && /usr/bin/sudo /usr/bin/chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+        && /usr/bin/echo "deb [arch=$(/usr/bin/dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && /usr/bin/sudo /usr/bin/apt update \
+        && /usr/bin/sudo /usr/bin/apt install gh -y
+fi
+
+latest_make_version=$(/usr/bin/curl "https://ftp.gnu.org/gnu/make/?C=M;O=D" -s \
+    | rg -o "href=\"make[^\"]*" \
+    | /usr/bin/sed "s/href=\"//g" \
+    | /usr/bin/head -2 \
+    | /usr/bin/tail -1)
+
+if [[ ! $(command -v make) ]]; then
+    /usr/bin/echo "[?] Installing Make via APT..."
+    /usr/bin/sudo /usr/bin/apt install make -y
+fi
+
+SCRIPTS_DIR="./setup_scripts"
+$SCRIPTS_DIR/apt.sh
+$SCRIPTS_DIR/cargo.sh
+$SCRIPTS_DIR/github.sh
+$SCRIPTS_DIR/go.sh
+$SCRIPTS_DIR/other.sh
