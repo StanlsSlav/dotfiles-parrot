31e30a455b30099f10c2589fb0e969b107d3cf30
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-07-28 14:11:47 +0200

    feat: skip neovim install when latest exists

--- setup_scripts/neovim.sh
+++ setup_scripts/neovim.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-echo "[?] Neovim install"
-/usr/bin/gh release download -R neovim/neovim -p nvim-linux64.tar.gz -D $HOME/Downloads
-/usr/bin/tar xzf $HOME/Downloads/nvim-linux64.tar.gz
-/usr/bin/sudo /usr/bin/mv $HOME/Downloads/nvim-linux64 /opt
+/usr/bin/echo "[?] Neovim install"
 
-/usr/bin/read -p "Copy the files instead of symlink?: [y]es " input
+SKIP_INSTALL=false
+
+latest_version=$(/usr/bin/gh release -R neovim/neovim view | /usr/bin/head -1 | /usr/bin/sed "s/title:\s*Nvim\s//")
+if [[ ! $(command -v nvim) ]]; then
+    current_version=$(/usr/local/bin/nvim --version | /usr/bin/head -1 | /usr/bin/sed "s/NVIM\s//")
+    SKIP_INSTALL=$(latest_version == current_version)
+fi
+
+if [[ ! $SKIP_INSTALL ]]; then
+    /usr/bin/gh release download -R neovim/neovim -p nvim-linux64.tar.gz -D $HOME/Downloads
+    /usr/bin/tar xzf $HOME/Downloads/nvim-linux64.tar.gz
+    /usr/bin/sudo /usr/bin/mv $HOME/Downloads/nvim-linux64 /opt
+fi
+
+/usr/bin/read -p "[?] Copy the files instead of symlink?: [y]es " input
 
 [[ -d $HOME/.config ]] \
     || /usr/bin/mkdir $HOME/.config
@@ -17,7 +28,7 @@ case $input in
     ;;
     
     *)
-	/usr/bin/ln -s $NVIM_CONF_DIR $HOME/.config/ 2>/dev/null
+	    /usr/bin/ln -s $NVIM_CONF_DIR $HOME/.config/ 2>/dev/null
     ;;
 esac
 
