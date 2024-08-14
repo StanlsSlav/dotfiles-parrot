26d53fa28a496aefa567d42624f8707010bfebef
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-08-14 18:11:03 +0200

    refactor: simplify keymap

--- configs/nvim/after/plugin/which_key.lua
+++ configs/nvim/after/plugin/which_key.lua
@@ -1,42 +1,46 @@
 local wk = require("which-key")
+local l = "<leader>"
 
 wk.add({
     {
-        group = "+file",
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "[f]ind" },
-        { "<leader>fr", "<cmd>Telescope oldfiles<cr>",   desc = "[r]ecent" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "[g]rep" },
-        { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "[b]uffers" },
-        { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "[h]elp" },
-        { "<leader>fs", "<cmd>Telescope symbols<cr>",    desc = "[s]ymbols" },
-        { "<leader>fn", "<cmd>enew<cr>",                 desc = "[n]ew" },
+        group = "file",
+        { l .. "ff", "<cmd>Telescope find_files<cr>", desc = "[f]ind" },
+        { l .. "fo", "<cmd>Telescope oldfiles<cr>",   desc = "[o]ld" },
+        { l .. "fr", "<cmd>Telescope registers<cr>",  desc = "[r]egisters" },
+        { l .. "fg", "<cmd>Telescope live_grep<cr>",  desc = "[g]rep" },
+        { l .. "fb", "<cmd>Telescope buffers<cr>",    desc = "[b]uffers" },
+        { l .. "fh", "<cmd>Telescope help_tags<cr>",  desc = "[h]elp" },
+        { l .. "fs", "<cmd>Telescope symbols<cr>",    desc = "[s]ymbols" },
+        { l .. "fn", "<cmd>enew<cr>",                 desc = "[n]ew" },
     },
+
     {
-        group = "+mason",
-        { "<leader>mm", "<cmd>Mason<cr>",       desc = "[m]ason" },
-        { "<leader>mu", "<cmd>MasonUpdate<cr>", desc = "[u]pdate" },
-        { "<leader>ml", "<cmd>MasonLog<cr>",    desc = "[l]og" },
+        group = "mason",
+        { l .. "mm", "<cmd>Mason<cr>",       desc = "[m]ason" },
+        { l .. "mu", "<cmd>MasonUpdate<cr>", desc = "[u]pdate" },
+        { l .. "ml", "<cmd>MasonLog<cr>",    desc = "[l]og" },
     },
+
     {
-        group = "+trouble",
-        { "<leadesc = der>td", "<cmd>Trouble diagnostics<cr>", desc = "[d]iagnostics" },
-        { "<leadesc = der>tl", "<cmd>Trouble lsp<cr>", desc = "[l]sp" },
+        group = "trouble",
+        { l .. "td", "<cmd>Trouble diagnostics<cr>", desc = "[d]iagnostics" },
+        { l .. "tl", "<cmd>Trouble lsp<cr>",         desc = "[l]sp" },
     },
 
-    { "<leader>g", "<cmd>Git<cr>",                  desc = "[g]it" },
-    { "<leader>u", "<cmd>UndotreeToggle<cr>",       desc = '[u]ndotree Toggle' },
-    { "<leader>l", "<cmd>Lazy<cr>",                 desc = "[l]azy" },
-    { "<leader>e", "<cmd>Ex<cr>",                   desc = "[e]xplorer" },
+    { l .. "g", "<cmd>Git<cr>",                  desc = "[g]it" },
+    { l .. "u", "<cmd>UndotreeToggle<cr>",       desc = '[u]ndotree Toggle' },
+    { l .. "l", "<cmd>Lazy<cr>",                 desc = "[l]azy" },
+    { l .. "e", "<cmd>Ex<cr>",                   desc = "[e]xplorer" },
 
-    { "<leader>J", desc = "append to current line" },
-    { "<leader>y", desc = "yank to sys clip" },
-    { "<leader>Y", desc = "yank line to sys clip" },
+    { l .. "J", desc = "append to current line" },
+    { l .. "y", desc = "yank to sys clip" },
+    { l .. "Y", desc = "yank line to sys clip" },
 
-    { "<leader>j", desc = "previous error" },
-    { "<leader>k", desc = "next error" },
+    { l .. "j", desc = "previous error" },
+    { l .. "k", desc = "next error" },
 
-    { "<leader>s", desc = "find and replace carret" },
-    { "<leader>Z", desc = "[Z]en mode" },
+    { l .. "s", desc = "find and replace carret" },
+    { l .. "Z", desc = "[Z]en mode" },
 })
 
 -- Show whick-key modal after this much milliseconds
