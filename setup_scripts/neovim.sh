6243ccbcaaf292a5346c0c1cf66518ee0cd97f10
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-08-14 17:33:46 +0200

    fix: properly determine version diff

--- setup_scripts/neovim.sh
+++ setup_scripts/neovim.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-/usr/bin/echo "[?] Neovim install"
+echo "[?] Neovim install"
 
 SKIP_INSTALL=false
 
-latest_version=$(/usr/bin/gh release -R neovim/neovim view | /usr/bin/head -1 | /usr/bin/sed "s/title:\s*Nvim\s//")
-if [[ ! $(command -v nvim) ]]; then
-    current_version=$(/usr/local/bin/nvim --version | /usr/bin/head -1 | /usr/bin/sed "s/NVIM\s//")
-    SKIP_INSTALL=$(latest_version == current_version)
+latest_version=$(/usr/bin/gh release -R neovim/neovim view | /usr/bin/head -1 | /usr/bin/sed "s/title:\sNVIM\s//")
+echo "$latest_version"
+
+if [[ $(command -v nvim) ]]; then
+    current_version=$(/usr/local/bin/nvim --version | /usr/bin/head -1 | /usr/bin/sed "s/NVIM\sv//")
+    SKIP_INSTALL=$([[ latest_version == current_version ]])
 fi
 
 if [[ ! $SKIP_INSTALL ]]; then
-    /usr/bin/gh release download -R neovim/neovim -p nvim-linux64.tar.gz -D $HOME/Downloads
-    /usr/bin/tar xzf $HOME/Downloads/nvim-linux64.tar.gz
-    /usr/bin/sudo /usr/bin/mv $HOME/Downloads/nvim-linux64 /opt
+    /usr/bin/gh release download -R neovim/neovim -p nvim-linux64.tar.gz -D /tmp
+    /usr/bin/tar xzf /tmp/nvim-linux64.tar.gz -C /tmp
+
+    /usr/bin/sudo /usr/bin/rm -rf /opt/nvim-linux64
+    /usr/bin/sudo /usr/bin/mv /tmp/nvim-linux64 /opt
 fi
 
 read -p "[?] Copy the files instead of symlink?: [y]es " input
@@ -26,9 +30,9 @@ case $input in
     'y')
         /usr/bin/cp -br $NVIM_CONF_DIR $HOME/.config/
     ;;
-    
+
     *)
-	    /usr/bin/ln -s $NVIM_CONF_DIR $HOME/.config/
+        /usr/bin/ln -s $NVIM_CONF_DIR $HOME/.config/
     ;;
 esac
 
