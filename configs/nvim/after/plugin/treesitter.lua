cc9d5e6abc3624630ed9ebb666406428efbb9064
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-08-14 22:34:42 +0200

    feat: treesitter autoinstall some parsers

--- configs/nvim/after/plugin/treesitter.lua
+++ configs/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,12 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { },
+    ensure_installed = {
+        "vimdoc",
+        "markdown",
+        "rust",
+        "bash",
+        "lua",
+        "python"
+    },
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true }
